768d1294863eeeb3490f26caad688f57c7cc0901
geroce17 <geroce@outlook.com>
2018-09-20 21:14:03 -0500

    Ejercicio de estilos

---
+++ Chacón Carrillo Daniel Gerónimo/10. CCDG - ejercicio/format/js/myMain.js
@@ -0,0 +1,14 @@
+function btnColorAzul() {
+    let el = document.getElementById('miEstilo');
+    el.setAttribute('href', 'css/style.min2.css');
+}
+
+function btnColorPrimario() {
+    let el = document.getElementById('miEstilo');
+    el.setAttribute('href', 'css/style.min.css');
+}
+
+function btnColorVerde() {
+    let el = document.getElementById('miEstilo');
+    el.setAttribute('href', 'css/style.min3.css');
+}
